7820624c22f1bb3cec153d9352aca12ff2a512f7
Rupam Kairi <rupamkairi@hotmail.com>
2023-06-05 19:05:19 +0530

    UI Improvement

--- components/Contact.vue
+++ components/Contact.vue
@@ -6,39 +6,50 @@
       <NeuButton label="Contact Me" smallLabel="Contact Me" />
     </div>
     <div class="mb-8">
-      <form action="" class="max-w-screen-sm mx-auto text-violet-500">
+      <form action="" class="max-w-screen-sm mx-auto">
         <div class="w-full mb-8">
-          <div class="w-full mb-8">
-            <label for="name" class="font-medium"
+          <div class="mb-8">
+            <label for="name" class="block mb-2 font-bold text-violet-500"
               >Where should we pick up our dicussion from?
-              <div class="text-sm">
+              <div class="text-sm font-medium">
                 Say something about your Project, Organisation, Expectation etc.
               </div>
             </label>
-            <textarea
-              placeholder="I want to build Website of *** for *** with ***"
-              class="w-full rounded-xl border-r-4 border-b-4 border-violet-500 py-2 px-4 outline-none"
-            ></textarea>
+
+            <NeuFormInput>
+              <textarea
+                placeholder="I want to build Website of *** for *** with ***"
+                class="w-full px-4 py-2 outline-none block"
+              ></textarea>
+            </NeuFormInput>
           </div>
-          <label for="name" class="font-medium">What should I call you?</label>
-          <input
-            id="name"
-            type="text"
-            placeholder="Your Name"
-            class="w-full rounded-xl border-r-4 border-b-4 border-violet-500 py-2 px-4 outline-none"
-          />
+          <div class="mb-8">
+            <label for="name" class="block mb-2 font-bold text-violet-500"
+              >What should I call you?</label
+            >
+            <NeuFormInput>
+              <input
+                id="name"
+                type="text"
+                placeholder="Your Name"
+                class="w-full py-2 px-4 outline-none"
+              />
+            </NeuFormInput>
+          </div>
         </div>
-        <div class="w-full mb-8">
-          <label for="email" class="font-medium"
+        <div class="mb-8">
+          <label for="email" class="block mb-2 font-bold text-violet-500"
             >How should I get back to you?
-            <div class="text-sm">Drop your email please.</div>
+            <div class="text-sm font-medium">Drop your email please.</div>
           </label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Or directly email me at rupamkairi@gmail.com"
-            class="w-full rounded-xl border-r-4 border-b-4 border-violet-500 py-2 px-4 outline-none"
-          />
+          <NeuFormInput>
+            <input
+              id="email"
+              type="email"
+              placeholder="Or directly email me at rupamkairi@gmail.com"
+              class="w-full py-2 px-4 outline-none"
+            />
+          </NeuFormInput>
         </div>
       </form>
     </div>
---
+++ components/NeuFormInput.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="relative">
+    <div
+      class="absolute -z-10 w-full h-full -bottom-1 border-b-4 -right-1 border-r-4 border-violet-500"
+    ></div>
+    <div class="block w-full border border-violet-500">
+      <slot></slot>
+    </div>
+  </div>
+</template>
